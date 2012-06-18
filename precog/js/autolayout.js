c835ea8330682aa8a923c446c62e56918d841d20
Taavi Burns <taavi@taaviburns.ca>
2012-06-18 17:21:03 -0400

    Provide autolayout function for impress.js presentations, for programmatic spatial locations.

---
+++ precog/js/autolayout.js
@@ -0,0 +1,12 @@
+function autolayout(step_function) {
+    step_function = step_function || function (step, steps) { return [step * 1200, 0]; };
+    var elements = document.getElementsByClassName('slide');
+    var eps = 1e-5;
+    var step;
+    for (var i = 0; i < elements.length; i++) {
+        step = step_function(i, elements.length);
+        for (var attr in step) {
+            elements[i].dataset[attr] = (Math.abs(step[attr]) < eps) ? 0 : step[attr];
+        }
+    }
+}
